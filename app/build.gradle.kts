eedfb71d44ea2aee75ef3eddcaeef8bb4784b093
Mateusz Roman Sulikowski <msulikowski96@gmail.com>
2026-06-03 15:38:41 +0200

    build: rebrand package to com.aistudio.metabolicpro
    Update application ID and versioning to reflect the new branding.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.kalkulatormetaboliczny.zxtqpy"
+    applicationId = "com.aistudio.metabolicpro"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.1"
+    versionCode = 1
+    versionName = "1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
