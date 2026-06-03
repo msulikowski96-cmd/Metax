85dd7178a77576c1488e219c676c9be27d8f4032
Mateusz Roman Sulikowski <msulikowski96@gmail.com>
2026-06-03 16:19:31 +0200

    chore: update application ID and version
    Rename package to com.company.metax and update version to 5.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.metabolicpro"
+    applicationId = "com.company.metax"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 5
+    versionName = "5.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
